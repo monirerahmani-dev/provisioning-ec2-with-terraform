e0322287f1eaabcc0a2e7a09b6adb0339a13a1b9
monirerahmani-dev <monirerahmani-dev@gmail.com>
2024-04-18 20:02:24 +0100

    modularize tf project

---
+++ modules/webserver/variables.tf
@@ -0,0 +1,8 @@
+variable vpc_id {}
+variable avail_zone {}
+variable env_prefix {}
+variable my_ip {}
+variable image_name {}
+variable instance_type {}
+variable subnet_id {}
+variable public_key_location {}
