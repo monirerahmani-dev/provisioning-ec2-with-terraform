0c3fdf92091b6cb061f65faa57204955e1210b63
monirerahmani-dev <monirerahmani-dev@gmail.com>
2024-04-18 15:09:28 +0100

    use default security group

--- main.tf
+++ main.tf
@@ -43,8 +43,7 @@ resource "aws_default_route_table" "main-rtb" {
     Name: "${var.env_prefix}-main-rtb"
   }
 }
-resource "aws_security_group" "myapp-sg" {
-  name = "myapp-sg"
+resource "aws_default_security_group" "default-sg" {
   vpc_id = aws_vpc.myapp-vpc.id
 
   ingress {
@@ -69,6 +68,6 @@ resource "aws_security_group" "myapp-sg" {
     prefix_list_ids = []
   }
   tags = {
-    Name: "${var.env_prefix}-sg"
+    Name: "${var.env_prefix}-default-sg"
   }
 }
