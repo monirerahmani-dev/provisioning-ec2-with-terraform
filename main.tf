6079fa7f3b9b071d999d0f346d832a5d15453aa5
monirerahmani-dev <monirerahmani-dev@gmail.com>
2024-04-18 15:03:32 +0100

    create security group

--- main.tf
+++ main.tf
@@ -6,6 +6,7 @@ variable vpc_cidr_blocks {}
 variable subnet_cidr_blocks {}
 variable avail_zone {}
 variable env_prefix {}
+variable my_ip{}
 
 resource "aws_vpc" "myapp-vpc" {
   cidr_block = var.vpc_cidr_blocks
@@ -40,5 +41,34 @@ resource "aws_default_route_table" "main-rtb" {
   }
   tags = {
     Name: "${var.env_prefix}-main-rtb"
+  }
+}
+resource "aws_security_group" "myapp-sg" {
+  name = "myapp-sg"
+  vpc_id = aws_vpc.myapp-vpc.id
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "TCP"
+    cidr_blocks = [var.my_ip]
+  }
+
+   ingress {
+    from_port = 8080
+    to_port = 8080
+    protocol = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    prefix_list_ids = []
+  }
+  tags = {
+    Name: "${var.env_prefix}-sg"
   }
 }
