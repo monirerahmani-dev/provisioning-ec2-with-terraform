1315765c80e6455c539aea0cf44367b102e62ca3
monirerahmani-dev <monirerahmani-dev@gmail.com>
2024-04-18 16:08:35 +0100

    automate creating key

--- main.tf
+++ main.tf
@@ -8,6 +8,7 @@ variable avail_zone {}
 variable env_prefix {}
 variable my_ip{}
 variable instance_type{}
+variable public_key_location{}
 
 resource "aws_vpc" "myapp-vpc" {
   cidr_block = var.vpc_cidr_blocks
@@ -84,10 +85,16 @@ data "aws_ami" "latest-amazon-linux-image" {
     values = ["hvm"]
   }
 }
-/*output name {
-  value = data.aws_ami.latest-amazon-linux-image
 
-}*/
+output ec2_public_ip {
+  value = aws_instance.myapp-server.public_ip
+}
+
+
+resource "aws_key_pair" "ssh-key" {
+  key_name = "server-key"
+  public_key = file(var.public_key_location)
+}
 
 resource "aws_instance" "myapp-server" {
   ami = data.aws_ami.latest-amazon-linux-image.id
@@ -98,7 +105,7 @@ resource "aws_instance" "myapp-server" {
   availability_zone = var.avail_zone
 
   associate_public_ip_address = true
-  key_name = "server-key-pair"
+  key_name = aws_key_pair.ssh-key.key_name
 
   tags = {
     Name: "${var.env_prefix}-server"
